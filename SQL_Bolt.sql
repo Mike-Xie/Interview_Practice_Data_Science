60a9adbdfa7beca0fdb05ea5a3676d1782ed2a20
Mike Xie <yuhuanxie@gmail.com>
2020-11-16 14:23:42 -0800

    filter and sort chapter 4

--- SQL_Bolt.sql
+++ SQL_Bolt.sql
@@ -4,30 +4,63 @@
                 
 --                     Find the movie with a row id of 6
 --                  ✓
-SELECT title FROM movies WHERE id = 6;
+SELECT title FROM movies 
+WHERE id = 6;
 --                     Find the movies released in the years between 2000 and 2010
 --                  ✓
-SELECT title FROM movies WHERE year BETWEEN 2000 AND 2010;                
+SELECT title FROM movies 
+WHERE year BETWEEN 2000 AND 2010;                
 --                     Find the movies not released in the years between 2000 and 2010
 --                  ✓
-SELECT title FROM movies WHERE year NOT BETWEEN 2000 AND 2010;                
+SELECT title FROM movies 
+WHERE year NOT BETWEEN 2000 AND 2010;                
 --                     Find the first 5 Pixar movies and their release year
 --                  ✓
-SELECT title, year FROM movies ORDER BY year ASC LIMIT 5;
+SELECT title, year FROM movies 
+ORDER BY year 
+ASC LIMIT 5;
 
 -- Exercise 3 — Tasks
 			
 			
                 
 --                     Find all the Toy Story movies
-SELECT * FROM movies WHERE title LIKE "%TOY STORY%";
+SELECT * FROM movies 
+WHERE title LIKE "%TOY STORY%";
                 
 --                     Find all the movies directed by John Lasseter
 -- do case insensitive with LIKE better than case sensitive with equal sign
-SELECT * FROM movies WHERE director LIKE "JOHN LASSETER";
+SELECT * FROM movies 
+WHERE director LIKE "JOHN LASSETER";
                 
 --                     Find all the movies (and director) not directed by John Lasseter
-SELECT * FROM movies WHERE director NOT LIKE "JOHN LASSETER"; 
+SELECT * FROM movies 
+WHERE director NOT LIKE "JOHN LASSETER"; 
                 
 --                     Find all the WALL-* movies
-SELECT * FROM movies WHERE title LIKE "WALL-%";
+SELECT * FROM movies 
+WHERE title LIKE "WALL-%";
+
+
+-- Exercise 4 — Tasks
+			
+			
+                
+--                     List all directors of Pixar movies (alphabetically), without duplicates
+--                  ✓
+SELECT DISTINCT director FROM movies 
+ORDER BY director ASC;                
+--                     List the last four Pixar movies released (ordered from most recent to least)
+SELECT * FROM movies 
+ORDER BY year DESC 
+LIMIT 4;                
+                
+--                     List the first five Pixar movies sorted alphabetically
+SELECT title FROM movies 
+ORDER BY title ASC 
+LIMIT 5;
+                
+--                     List the next five Pixar movies sorted alphabetically
+SELECT title FROM movies
+ORDER BY title ASC
+LIMIT 5 OFFSET 5;
