99b8a24afbc694f19a9e50bb4ddf3a27866ca71e
Mike Xie <yuhuanxie@gmail.com>
2020-11-16 14:11:35 -0800

    chapter 3 done

--- SQL_Bolt.sql
+++ SQL_Bolt.sql
@@ -15,3 +15,19 @@ SELECT title FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
 --                  ✓
 SELECT title, year FROM movies ORDER BY year ASC LIMIT 5;
 
+-- Exercise 3 — Tasks
+			
+			
+                
+--                     Find all the Toy Story movies
+SELECT * FROM movies WHERE title LIKE "%TOY STORY%";
+                
+--                     Find all the movies directed by John Lasseter
+-- do case insensitive with LIKE better than case sensitive with equal sign
+SELECT * FROM movies WHERE director LIKE "JOHN LASSETER";
+                
+--                     Find all the movies (and director) not directed by John Lasseter
+SELECT * FROM movies WHERE director NOT LIKE "JOHN LASSETER"; 
+                
+--                     Find all the WALL-* movies
+SELECT * FROM movies WHERE title LIKE "WALL-%";
